2a53c05a126096d664b3e729e1e473cd59f3633f
2ofiaLagos <soflagosleiva@crackthecode.la>
2025-09-16 20:14:47 -0500

    Update inciair.js

--- inciair.js
+++ inciair.js
@@ -4,7 +4,7 @@ let contraseña = document.getElementById("Contra").value
 
 if(user == "Sofia" && contraseña == "Lagos"
 ){
-    document.body.innerHTML ="Hola Sofía"
+    document.body.innerHTML ="Hola" + user
     document.body.style.color = "white"
     document.body.className= "fugaz-one-regular"
     document.body.style.fontSize = "large"
